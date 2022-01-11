20fc9c166af9feb83268b019e38118899f24c1f3
qalmaqihir <supine.me@gmail.com>
2022-01-11 12:48:46 +0600

    several methods have been added

--- SingledLinkedList/SingledLinkedList.py
+++ SingledLinkedList/SingledLinkedList.py
@@ -66,6 +66,142 @@ class SingalLinkedList:
             data = int(input("Enter the element to be insearted: "))
             self.insert_at_end(data)
 
+    def insert_after(self,data,x):
+        temp=Node(data)
+        p=self.start
+        while p is not None:
+            if p.info==x:
+                break
+            p=p.link
+        if p is None:
+            print(x, " Not present in the list")
+        else:
+            temp.link=p.link
+            p.link=temp
+
+    def insert_before(self,data,x):
+        # if the list is empty
+        if self.start is None:
+            print("List is empty")
+            return
+        # if x is the first node in the list
+        if x==self.start.info:
+            temp=Node(data)
+            temp.link=self.start
+            self.start=temp
+            return
+        # Find the precedant node to x
+        p = self.start
+        while p.link is not None:
+            if p.link.info==x:
+                break
+            p=p.link
+
+        if p.link is None:
+            print(x, " Not present in the list")
+        else:
+            temp=Node(data)
+            temp.link=p.link
+            p.link=temp
+
+    def insert_at_position(self,data,k):
+        # if the data is to be inserted in the start  of the list
+
+        if k==1:
+            temp=Node(data)
+            temp.link=self.start
+            self.start=temp
+            return
+        p=self.start
+        i=1
+        while i<k-1 and p is not None: # find a reference to k-1 node
+            p=p.link
+            i+=1
+        if p in None:
+            print("You can insert up to the position ",i)
+        else:
+            temp=Node(data)
+            temp.link=p.link
+            p.link=temp
+            return
+
+
+    def delete_first_node(self):
+        if self.start is None:
+            return
+
+        self.start=self.start.link
+
+    def delete_last_node(self):
+        if self.start is None:
+            return
+        if self.start.link is None:
+            self.start=None
+        p=self.start
+        while p.link.link is not None:
+            p=p.link
+        p.link=None
+
+    def delete_node(self,x):
+
+        if self.start is None:
+            print("List is empty")
+            return
+        # deletion of the first node
+        if self.start.info ==x:
+            self.start = self.start.link
+
+        p=self.start
+        while p.link is not None:
+           if  p.link.info==x:
+               break
+           p=p.link
+        if p.link is None:
+            print(x, " is not in the list")
+        else:
+            p.link=p.link.link
+
+    def reverse_list(self):
+        prev=None
+        p=self.start
+        while p is not None:
+            next= p.link
+            p.link=prev
+            prev=p
+            p=next
+        self.start=prev
+
+    def bubble_sort_exdata(self):
+        end=None
+        while end!=self.start.link:
+            p=self.start
+            while p.link !=end:
+                q=p.link
+                if p.info>q.info:
+                    q.info,p.info=p.info, q.info
+                p=p.link
+            end=p
+
+    def bubble_sort_exlinks(self):
+        end = None
+        while end!=self.start.link:
+            r=p=self.start
+            while p.link!=end:
+                q=p.link
+                if p.info>q.info:
+                    p.link=q.link
+                    p.link=p
+                    if p!=self.start:
+                        r.link=q
+                    else:
+                        self.start=q
+                    p,q=q,p
+                r=p
+                p=p.link
+            end=p
+
+
+
 
 
 
@@ -81,8 +217,17 @@ listt= SingalLinkedList()
 
 listt.create_list()
 listt.display_list()
-listt.insert_at_end(-1)
-listt.insert_in_beginning(0)
-listt.search(-2)
-listt.count_nodes()
+# listt.insert_at_end(-1)
+# listt.insert_in_beginning(0)
+# listt.search(-2)
+# listt.count_nodes()
+# listt.display_list()
+# listt.insert_after()
+# listt.insert_after(-1,0)
+# listt.display_list()
+# listt.insert_before(3,5)
+# listt.display_list()
+
+listt.reverse_list()
+print("revered List \n")
 listt.display_list()
